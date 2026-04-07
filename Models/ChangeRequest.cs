2020a7b414fafbe868e58e5150989b3f39f94fbd
girbix <105404973+girbix@users.noreply.github.com>
2026-04-07 16:37:54 +0200

    Add ChangeRequest model with relevant properties

---
+++ Models/ChangeRequest.cs
@@ -0,0 +1,12 @@
+namespace NOC_NG.Models;
+
+public class ChangeRequest : Ticket
+{
+    public string? Justification { get; set; }
+    public string? ImplementationPlan { get; set; }
+    public string? RiskAnalysis { get; set; }
+    public string? TicketGrade { get; set; } // Standard / Normal / Emergency
+    public string? Rca { get; set; }
+    public DateTime? ChangeWindow { get; set; }
+    public bool IsApproved { get; set; } = false;
+}
