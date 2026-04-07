6e3268404c97fb7c9af892702860a7f8e729e92f
girbix <105404973+girbix@users.noreply.github.com>
2026-04-07 17:24:43 +0200

    Delete Models directory

--- Models/Incident.cs
+++
@@ -1,8 +0,0 @@
-namespace NOC_NG.Models;
-
-public class Incident : Ticket
-{
-    public string? Category { get; set; }
-    public string? Resolution { get; set; }
-    public DateTime? ClosedDate { get; set; }
-}
